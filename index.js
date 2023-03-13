dcdb5e15e8c2c17687edcf1ea902d4168c644708
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-14 00:43:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(f){
   // Write your code here!
-}
+  let test;
+  if (f<= 400) {
+    return "This one is on me!"
+  } 
+  else if (f> 2000  && f <2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if(f>= 2501){
+    return "No can do."  }
+  }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  var locationCheck = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return locationCheck;
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(message){
   // Write your code here!
+  let charm;
+
+  switch(message){
+
+  case 'generous':
+    charm = 'Thank you so much.'
+  break;
+
+  case 'not as generous':
+    charm = 'Thank you.'
+  break;
+
+  default:
+    charm = 'Bye.'
+
+  }
+  return charm;
 }
